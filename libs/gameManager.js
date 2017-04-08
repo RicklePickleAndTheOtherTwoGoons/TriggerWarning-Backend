6a6191d95b807fce0f109199ff60472b93a3c134
Chandler Willoughby <chandler243@gmail.com>
2017-04-08 07:48:34 -0400

    Checkpoint

--- libs/gameManager.js
+++ libs/gameManager.js
@@ -50,16 +50,62 @@ function newGame(socket, cardsets, playerLimit, scoreLimit, callback) {
     });
 }
 
-function roundBegin(socket) {
+function gameJoin(socket, roomCode, callback) {
+    Game.findOne({roomCode:roomCode}, function(err, game) {
+        if (err) console.log(err);
+        if (game==null) {
 
+        } else {
+            redis.sadd('game:'+game._id+":activeusers",socket.id);
+            callback(game)
+        }
+    })
+}
+
+function gameLeave(socket, game) {
+    redis.srem('game:'+game._id+":activeusers", socket.id)
+}
+
+function gameStart(socket, game) {
+    redis.spop("game:"+game._id+":whitecards", 10, function(err, cards) {
+        if (err) console.log(err);
+        socket.emit('firstHand', cards)
+    })
 }
 
 module.exports = function(io) {
     io.on('connection', function (socket) {
+        var currentGame = {};
         socket.on('gameCreate', function(data) {
             newGame(socket, data.cardSets, data.playerLimit, data.scoreLimit, function(game) {
-                socket.emit('gameCreated', game)
+                socket.emit('gameCreated', game);
+                currentGame = game;
+                socket.join(game._id)
             });
+        });
+        socket.on('gameJoin', function(data) {
+            console.log('Recieved game join request');
+            gameJoin(socket, data.roomCode, function(game) {
+                if (game==null) {
+                    socket.emit('error', {text:"Bad room code"})
+                } else {
+                    socket.emit('targetGame',game);
+                    currentGame = game;
+                    socket.join(game._id);
+                    io.in(game._id).clients(function(err, clients) {
+                        if (clients.length == game.playerLimit) {
+                            io.in(game._id).emit('gameFull')
+                        }
+                    })
+                }
+
+            })
+        });
+        socket.on('gameStart', function() {
+            console.log('Game start was requested')
+        })
+        socket.on('disconnect', function(data) {
+            gameLeave(socket, currentGame)
         })
     });
 };
