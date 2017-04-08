4b70c85a1432ce93f2201abbbeee33fd7284e594
Chandler Willoughby <chandler243@gmail.com>
2017-04-07 21:43:12 -0400

    Card model

--- models/game.js
+++ models/game.js
@@ -1,3 +1,24 @@
 /**
  * Created by chandler on 4/7/17.
  */
+var mongoose = require('mongoose');
+var gameSchema = mongoose.Schema({
+    activeCardSets: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Cardset'}],
+    whiteCards: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Card'}],
+    blackCards: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Card'}],
+    rounds: [{
+        roundNumber: {type:number},
+        blackCard: {type:mongoose.Schema.Types.ObjectId, ref: 'Card'},
+        userSubmissions: [{
+            user: {type: string},
+            whiteCard: {type:mongoose.Schema.Types.ObjectId, ref: 'Card'},
+            czarPick: {type:bool}
+        }]
+    }],
+    roundStart: {type:date, default: Date.now()},
+    roundEnd: {type: date},
+    scoreLimit: {type:number, default: 10},
+    playerLimit: {type:number, default: 8},
+    completed: {type:bool}
+});
+module.exports = mongoose.model('Game', gameSchema);
